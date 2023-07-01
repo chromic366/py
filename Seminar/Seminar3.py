edc720f08502c5c14f991606918292e9cdd95d0d
chromic <chromic366@gmail.com>
2023-07-01 18:08:33 +0500

    Task2

--- Seminar/Seminar3.py
+++ Seminar/Seminar3.py
@@ -1,14 +1,30 @@
 # Дан список чисел. Определите, сколько в нем
 # встречается различных чисел.
 
-list_1  = [1, 2, 3, 1, 1, 5, 10, 20, 20, 30]
+# list_1  = [1, 2, 3, 1, 1, 5, 10, 20, 20, 30]
+#
+# print(len(set(list_1)))
+#
+# my_list = [1, 1, 2, 0, -1, 3, 4, 4, 3, -3, 0, 10, 11, 11]
+#
+# for i in my_list:
+#     if my_list.count(i) > 1:
+#         for _ in range(my_list.count(i) - 1):
+#             my_list.remove(i)
+# print(len(my_list))
 
-print(len(set(list_1)))
+# Дана последовательность из N целых чисел и число
+# K. Необходимо сдвинуть всю последовательность
+# (сдвиг - циклический) на K элементов вправо, K –
+# положительное число.
 
-my_list = [1, 1, 2, 0, -1, 3, 4, 4, 3, -3, 0, 10, 11, 11]
+lst = [1, 2, 3, 4, 5]
+k = 2
+for i in range(k):
+    lst.insert(0, lst.pop())
+print(lst)
 
-for i in my_list:
-    if my_list.count(i) > 1:
-        for _ in range(my_list.count(i) - 1):
-            my_list.remove(i)
-print(len(my_list))
+lst = [1, 2, 3, 4, 5]
+a1 = lst[-k:]
+a2 = lst[:-k]
+print(a1 + a2)
