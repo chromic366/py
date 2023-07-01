75aafa6846f3a5ac6294ef39e55d214327184c84
chromic <chromic366@gmail.com>
2023-07-01 18:15:07 +0500

    Task3

--- Seminar/Seminar3.py
+++ Seminar/Seminar3.py
@@ -18,13 +18,28 @@
 # (сдвиг - циклический) на K элементов вправо, K –
 # положительное число.
 
-lst = [1, 2, 3, 4, 5]
-k = 2
-for i in range(k):
-    lst.insert(0, lst.pop())
-print(lst)
+# lst = [1, 2, 3, 4, 5]
+# k = 2
+# for i in range(k):
+#     lst.insert(0, lst.pop())
+# print(lst)
+#
+# lst = [1, 2, 3, 4, 5]
+# a1 = lst[-k:]
+# a2 = lst[:-k]
+# print(a1 + a2)
 
-lst = [1, 2, 3, 4, 5]
-a1 = lst[-k:]
-a2 = lst[:-k]
-print(a1 + a2)
+
+# Напишите программу для печати всех уникальных
+# значений в словаре.
+
+list_1 = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+          {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "},
+          {" VIII":" S007 "}]
+
+set_n = set()
+
+for dct in list_1:
+    set_n.add(str(*dct.values()).strip())
+
+print(set_n)
