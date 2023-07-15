821a4c568b6db916c31b6de4806018829ed51e32
chromic <chromic366@gmail.com>
2023-07-15 16:41:06 +0500

    Task2

--- Seminar/Seminar7_DZ.py
+++ Seminar/Seminar7_DZ.py
@@ -13,11 +13,29 @@
 # и “Пам парам”, если с ритмом все не в порядке
 
 
-alp = "аеёиоуыэюя"
-word_sug = input().split()
-vowel_letters = [sum([True for j in word if j.lower() in alp]) for word in word_sug]
+# alp = "аеёиоуыэюя"
+# word_sug = input().split()
+# vowel_letters = [sum([True for j in word if j.lower() in alp]) for word in word_sug]
+#
+# if all(vowel_letters) and len(set(vowel_letters)) == 1:
+#     print("Парам пам-пам")
+# else:
+#     print("Пам парам")
 
-if all(vowel_letters) and len(set(vowel_letters)) == 1:
-    print("Парам пам-пам")
-else:
-    print("Пам парам")
+
+# Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
+# столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+# почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
+# ровно два аргумента, как, например, у операции умножения.
+
+
+def print_operation_table(operation, num_rows, num_columns):
+    for i in range(1 , num_rows + 1):
+        list = []
+        for j in range(1, num_columns + 1):
+            list.append(i * j)
+        print(*list)
+
+print_operation_table(lambda x, y: x * y, int(input('row: ')), int(input('column: ')))
